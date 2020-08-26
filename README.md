bd0c884164c7701adf063c2ea68f3b24509884d6
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-08-26 17:32:58 -0400

    added first version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # random-slide
 Scrapes Slideshare and pulls a random slide.
+
+## How it works
+This searches [slideshare.net](https://slideshare.net) with [random English words](https://github.com/dwyl/english-words), pulls a random result, and chooses a random slide
+from the given slide show. The response gives basic information on the slideshow as well as the slide URL.
+
+# Inspired by
+
+- [GoodSlides](https://twitter.com/GoodSlides) - Neil Cicierega's bot of similar fashion
+- [This Tom Scott video](https://www.youtube.com/watch?v=zp4BMR88260) 
+
+## Installation
+0. Have a recent version of Nodejs installed.
+1. Install this code to your system and install dependencies via ``npm i``.
+2. Run ``node .``
+3. Enjoy!
---
+++ index.js
@@ -0,0 +1,40 @@
+const got = require("got");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+grab();
+
+function grab() {
+    var words = fs.readFileSync("./words.txt").toString().split(`\n`);
+    var randomWord = words[Math.floor(Math.random()*words.length)];
+    
+    var url = "https://www.slideshare.net/search/slideshow?searchfrom=header&q=" + randomWord;
+    got(url).then(function(response) {
+        var $ = cheerio.load(response.body);
+        if ($(".notice-no-results")[0]) {
+            grab();
+            return;
+        } else {
+            var items = $(".searchResults .columns ul li div a");
+            var randomSlide = items[Math.floor(Math.random()*items.length)];
+            if (randomSlide.attribs.href) {
+                var url2 = "https://www.slideshare.net" + randomSlide.attribs.href;
+                got(url2).then(function(response) {
+                    var $ = cheerio.load(response.body);
+                    var max = parseInt($("#total-slides")[0].children[0].data) - 1;
+                    var rs = Math.floor(Math.random() * max);
+                    var imgsrc = $(".slide_image")[0].attribs.src;
+                    var final = imgsrc.replace("-1-", "-" + rs + "-");
+                    var title = $(".j-title-breadcrumb").text();
+                    var author = $(".author-text h2 a span").text();
+                    console.log(final);
+                    console.log("================");
+                    console.log("title: " + title);
+                    console.log("url: " + url2);
+                    console.log("author: " + author);
+                    console.log("query: " + randomWord);
+                })
+            }
+        }
+    })
+}
